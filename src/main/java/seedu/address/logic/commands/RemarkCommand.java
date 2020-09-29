a154ffa08ca533104c5722d0aef99d316c029c6e
drake251200 <drake251@comp.nus.edu.sg>
2020-09-29 18:25:37 +0800

    Fix4

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -4,11 +4,13 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
 import java.util.List;
 
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Remark;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 
